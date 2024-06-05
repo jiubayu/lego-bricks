644acb494ae3fd0ecd0fe43f2ae9fe43ca95bf8e
Tianbao <tb_yang@trip.com>
2024-06-05 17:49:24 +0800

    add finalpage component

---
+++ src/components/FinalPage/FinalPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="final-page" :style="page && page.props"> 
+    <div v-for="item in components" :key="item.id" :id="`component-${item.id}`">
+      <component :is="item.name" v-bind="item.props"></component>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import { PageData, ComponentData } from '../../defaultProps';
+export default defineComponent({
+  name: 'FinalPage',
+  props: {
+    page: {
+      type: Object as PropType<PageData>,
+    },
+    components: {
+      type: Object as PropType<ComponentData[]>,
+      required: true,
+    }
+  }
+})
+</script>
--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,9 @@ export {
 import LText from './components/LText';
 import LImage from './components/LImage';
 import LShape from './components/LShape';
+import FinalPage from './components/FinalPage';
 
-const components = [LText, LImage, LShape];
+const components = [LText, LImage, LShape, FinalPage];
 
 const install = (app: App) => {
   components.forEach((component) => {
@@ -23,7 +24,7 @@ const install = (app: App) => {
   });
 };
 
-export {LText, LImage, LShape, install};
+export {LText, LImage, LShape, FinalPage, install};
 export default {
   install,
 };
